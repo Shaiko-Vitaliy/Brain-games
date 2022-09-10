e70478a596262c047950df2a02402878416989ac
Shaiko-Vitaliy <shaiko2586@gmail.com>
2022-09-10 23:41:33 +0300

    rename Great.java

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -15,7 +15,7 @@ public class App {
         Engine.scanner.nextLine();
         switch (numGame) {
             case 1:
-                Great.sayHello();
+                Cli.playGame();
                 break;
             case 2:
                 Even.playGame();
