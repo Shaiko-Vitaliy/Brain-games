7c71779fbe2c6c254e871917a63f3e22cbbf87b5
Shaiko-Vitaliy <shaiko2586@gmail.com>
2022-09-11 19:55:20 +0300

    fixed division by zero

--- src/main/java/hexlet/code/game/GCD.java
+++ src/main/java/hexlet/code/game/GCD.java
@@ -11,8 +11,8 @@ public class GCD {
         App.sayHello();
         System.out.println("Find the greatest common divisor of given numbers.");
         for (int i = 0; i < Engine.quantityAround; i++) {
-            var num1 = RandomUtils.generateRandomNums(RandomUtils.minNums, RandomUtils.maxNums);
-            var num2 = RandomUtils.generateRandomNums(RandomUtils.minNums, RandomUtils.maxNums);
+            var num1 = RandomUtils.generateRandomNums(1, RandomUtils.maxNums);
+            var num2 = RandomUtils.generateRandomNums(1, RandomUtils.maxNums);
             var expresion = num1 + " " + num2;
             var resultComparisonQuestionAndAnswer = Engine.getResultGame(num1, num2, expresion);
             if (!resultComparisonQuestionAndAnswer) {
